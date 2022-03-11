7f33dcbc2afc27169e44d41dce921edfa83bcc1a
root <chidumagambata@gmail.com>
2022-03-10 16:27:45 -0800

    commit task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-return (0);
-# main-entery point
- #Return: 0 after printing the function
+#include <stdio.h>
+#main - entery point
+#Return: 0 after printing the function
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
