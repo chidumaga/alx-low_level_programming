20e62115ea02436904a3d5d8e89f97d38f8a7650
root <chidumagambata@gmail.com>
2022-03-12 05:17:21 -0800

    commit for task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entery point
+ *
+ * My hello, printf task
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+	printf("Size of a char: %dbyte(s)\n",sizeof(char));
+
+	printf("Size of an int: %dbyte(s)\n",sizeof(int));
+
+	printf("Size of a long int: %dbyte(s)\n",sizeof(long int));
+
+	printf("Size of a float: %dbyte(s)\n",sizeof(float));
+
+	return (0);
+}
+
+
