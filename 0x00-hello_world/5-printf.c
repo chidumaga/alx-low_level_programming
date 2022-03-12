ada4ed40a80032cc75b99d91109e005dd09f4f92
root <chidumagambata@gmail.com>
2022-03-12 05:02:16 -0800

    commit for task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *
+ *  * main - entery point
+ *
+ *   * My hello, printf task
+ *
+ *    * Return: 0 after printing the function
+ *
+ *    */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
