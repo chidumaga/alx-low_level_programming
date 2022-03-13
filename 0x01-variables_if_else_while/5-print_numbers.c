42cac8faffdea4bca3f92dc157d5e73075130ab5
root <chidumagambata@gmail.com>
2022-03-12 23:52:20 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main-entery point
+ *
+ * Return: Return: Always(sucess/correct)
+ *
+ */
+int main(void)
+{
+	int d;
+
+	for ( d= 0; d < 10; d++)
+	{
+		printf("%d", d);
+	}
+	printf("\n");
+	return (0);
+}
