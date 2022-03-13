c9ffb5ba5df5f3ab37489e98f0ec8e85175a7f01
chidumaga <40515830+chidumaga@users.noreply.github.com>
2022-03-13 03:06:07 -0400

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,12 @@
 int main(void)
 {
 	char alphabet;
-	
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 	putchar(alphabet);
 	}
 	
-	putchar('\n');
+putchar('\n');
 	
-        return (0);
+return (0);
 }
