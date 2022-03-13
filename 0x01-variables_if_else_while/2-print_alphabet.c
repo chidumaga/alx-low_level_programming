74edfd01409901124aeb3a3f1a5da9f04e8622ce
root <chidumagambata@gmail.com>
2022-03-12 22:59:21 -0800

    commit for task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *
+ * main-entery point
+ *
+ * Return: Return: Always(sucess/correct)
+ *
+ */
+int main(void)
+{
+	char alphabet;
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+      return (0);
+}
