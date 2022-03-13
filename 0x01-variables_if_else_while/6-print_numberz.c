9daacaa145c9f86270b11553972aebaeda11e610
root <chidumagambata@gmail.com>
2022-03-13 00:04:12 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main-entery point
+ *
+ * Return: Return: Always(sucess/correct)
+ *
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 48; d<= 57; d++)
+	{
+		putchar (d);
+	}
+
+	putchar ("\n");
+	return (0);
+}
