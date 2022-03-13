cb2c97d38560763aa69f62b92da9988541d824dd
root <chidumagambata@gmail.com>
2022-03-13 00:13:36 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main-entery point
+ *
+ * Return: Return: Always(sucess/correct)
+ *
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 122; alphabet >== 97; alphabet--)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}
