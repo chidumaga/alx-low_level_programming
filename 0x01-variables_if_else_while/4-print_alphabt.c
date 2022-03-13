0910c372539cb01b66529588e43c71e35959a1e5
root <chidumagambata@gmail.com>
2022-03-12 23:40:44 -0800

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ *
+ * main-entery point
+ *
+ * Return: Return: Always(sucess/correct)
+ *
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+		else if (alphabet == 'e')
+			continue;
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
