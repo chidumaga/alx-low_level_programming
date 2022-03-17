67290100014ded50344144eb1d8a22db60cf926f
root <chidumagambata@gmail.com>
2022-03-17 01:34:41 -0700

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,10 +3,6 @@
 
 #include <stdio.h>
 
-void positive_or_nagative(int i);
-int largest_number(int a, int b, int c);
-#include <stdio.h>
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
