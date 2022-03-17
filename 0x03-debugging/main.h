3602a890eadf76859030b9a0a6edd4cb8bc6814e
root <chidumagambata@gmail.com>
2022-03-17 00:41:51 -0700

    debugging tast 1

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_nagative(int i);
+int largest_number(int a, int b, int c);
+#include <stdio.h>
+
+void positive_or_nagative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_days(int month, int day);
+
+#endif
