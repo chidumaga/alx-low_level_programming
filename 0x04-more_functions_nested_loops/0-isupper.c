60fd0bb7ff7313bf26bc6c098905148bd3e38ed7
root <chidumagambata@gmail.com>
2022-03-17 16:25:05 -0700

    commit for 0-isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *_isupper - evalue if letter is uppercase .
- *@c: print int
- *Return: Always 0.
+ * _isupper - evalue if letter is uppercase.
+ * @c: print int
+ * Return: Always 0.
  */
 
 int _isupper(int c)
