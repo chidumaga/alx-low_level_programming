c8c5753ab3fe499b40e049ff995a0de3ee89b989
root <chidumagambata@gmail.com>
2022-03-17 01:04:45 -0700

    task 01 for debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,25 @@
+/**
+ * main - causes and infinite loop
+ * 
+ * Return: 0
+ */
+
+int main(void)
+{
+int i:
+
+printf("infiniate loop incoming :(\n");
+
+i = 0;
+
+/*
+ * while (i < 10)
+ * {
+ * infinite loop - No increase of variable
+ * putchar(i);
+ }
+*/
+
+printf("infinite loop avoided: \\o/\n");
+return (0);
+}
