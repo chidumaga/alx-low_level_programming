35e65ec53524aa172b0115225dda7576b5fdc1c4
root <chidumagambata@gmail.com>
2022-03-18 00:14:00 -0700

    commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,9 +2,7 @@
 /**
  * _isdigit - if is a digit
  * @c: print int
- * Return: Always 0.
 */
-
 int _isdigit(int c)
 {
 if (c >= 48 && c <= 57)
@@ -12,7 +10,6 @@ if (c >= 48 && c <= 57)
 return (1);
 }
 else
-
 {
 return (0);
 }
