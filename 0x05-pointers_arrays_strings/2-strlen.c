dfbc44bc66ce0101bc224656fd5b6e4f5331069a
chidumaga <40515830+chidumaga@users.noreply.github.com>
2022-03-21 20:15:14 -0400

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strien - length of a string
+ * _strlen - length of a string
  * @s: A pointer to an int that will be changed/updated
  *
  * Return: void that means our answer is correct
