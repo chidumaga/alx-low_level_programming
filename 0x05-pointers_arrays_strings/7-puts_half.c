47db7bc1389cc8bf4ebdeb716fb95f541625491e
root <chidumagambata@gmail.com>
2022-03-22 02:38:25 -0700

    commit for task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * put_half - print every other character of string
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: void which our answer is correct
+ */
+void put_half(char *str)
+{
+	int i, last;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	last = (i + 1) / 2;
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i];
+	}
+	_putchar ('\n');
+}
