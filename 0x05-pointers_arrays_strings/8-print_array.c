1ebc25eb0cf118c2dd2ca1d5f64faccc499d2e5f
root <chidumagambata@gmail.com>
2022-03-22 02:54:50 -0700

    commit for task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include  "main.h"
+#include <stdio>
+/**
+ * print_array - print n elements of an array of integer
+ * @a: A pointer to an int that will be updated/changed
+ * @n: return value n
+ * Return: void which means our answer is correct
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (1 < n)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	printf("\n");
+}
