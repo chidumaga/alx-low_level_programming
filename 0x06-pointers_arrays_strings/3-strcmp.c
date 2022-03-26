2e685a8768b0eaf7182e8c6b9f302689b896a34d
chidumaga <40515830+chidumaga@users.noreply.github.com>
2022-03-26 11:12:23 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s2: A pointer to a character that will be changed
  * Return: dest
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	char *str_one = s1;
 	char *str_two = s2;
